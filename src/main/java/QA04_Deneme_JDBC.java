15c566d361a8a64ba729472b9b64db7edd71f471
techproed <burcupiaf@gmail.com>
2023-03-17 14:01:39 -0500

    qa04 jdbc class

---
+++ src/main/java/QA04_Deneme_JDBC.java
@@ -0,0 +1,16 @@
+public class QA04_Deneme_JDBC {
+    public static void main(String[] args) {
+
+//        JDBC: Mnager vasitasi ile bizi database yonlendiriyor.
+//
+//        Driver:Bu arayuz veritabani sunucusu ile olan iletisimi idare eder.
+//
+//                Connection: Bu arayuz bir veritabani ile iletisim kurmak icin tum yontemleri icerir.Connection iletisim baglamini temsil eder.
+//
+//                ResultSet: JDBC kullanarak veri cekme islemi sonrasinda veri listelemek icin ResultSet sinifi kullanilir.
+//
+//        Database 'e full erisim vermek risklidir genelde sinirli yetkiler verilir.
+//
+//        Genelde select komutu ile calisir.
+    }
+}
